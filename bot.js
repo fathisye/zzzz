8bfa99d4b7ec057b0fc9630712c2041da55241d8
fathisye <47281515+fathisye@users.noreply.github.com>
2019-02-09 16:41:22 +0200

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,16 @@
+﻿const Discord = require('discord.js');
+const client = new Discord.Client();
+console.log("Scrpit By Dream");
+
+
+client.on("ready", (.) => {
+let channel =     client.channels.get("542328854199664661")
+setInterval(function() {
+channel.send(`farmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarm
+farmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarm
+farmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarm
+farmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarmfarm`);
+}, 30)
+})
+
+client.login(process.env.BOT_TOKEN);
